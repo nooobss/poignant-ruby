77f757cce2f2584cfbe2cabbbb1ee5ba22a5d8e7
nooop <harisnooop@gmail.com>
2026-02-15 22:45:11 +0700

    mencetak array tanpa bikin index i

--- quick-ride.rb
+++ quick-ride.rb
@@ -1,3 +1,10 @@
+=begin
+
 exit unless "restaurant".include? "aura"
 5.times {print "hello Solo \n"}
 
+=end
+
+
+# kode ruby seperti dialog dalam bahasa inggris
+["toast", "cheese", "wine"].each { |food| print food.capitalize + " "}
