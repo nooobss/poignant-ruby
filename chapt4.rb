eaefb056eaeac4c44a6bc38ec25b60998e690c79
nooop <harisnooop@gmail.com>
2026-02-17 23:35:49 +0700

    menulis hash dengan 2 arah

--- chapt4.rb
+++ chapt4.rb
@@ -1,3 +1,5 @@
+=begin
+
 # nil
 # nil mewakili kekosongan. bukan angka nol. nol adalah angka
 gelas = nil
@@ -10,3 +12,58 @@ end
 # atau bisa ditulis
 
 puts "gelas kosong" unless gelas
+
+=end
+
+=begin
+
+hujan = true
+if hujan
+    puts 'jalanan basah ges'
+end
+
+at_hotel = true
+email = if at_hotel
+    'why@hotel.com'
+    else
+    'gogo@gmail.com'
+end
+puts email
+
+=end
+
+=begin
+
+at_hotel = true
+email = if at_hotel
+  address = 'why'
+  address << '@hotel'
+  address << '.com'
+else
+  'guest@hotel.com'
+end
+puts email
+# << : fungsinya seperti + , concatenate/append
+
+=end
+
+# Hash
+# key-value pair
+# dibuat dengan 2 cara, hash rocket dan symbol
+
+# hash rocket
+# key berupa string
+jokoui = {
+    "skill" => "ngutang",
+    "kelemahan" => "ijazah"
+}
+
+p jokoui
+
+# symbol
+praowo = {
+    skill: "nuduh asenk",
+    kelemahan: "lantai wc" 
+}
+
+p praowo
