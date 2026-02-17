36ed439b406b4e468f892e1b09cf4bd7e966497b
nooop <harisnooop@gmail.com>
2026-02-17 11:45:57 +0700

    memahami conditional dengan nil

--- chapt4.rb
+++ chapt4.rb
@@ -2,3 +2,11 @@
 # nil mewakili kekosongan. bukan angka nol. nol adalah angka
 gelas = nil
 puts gelas
+
+unless gelas
+    puts "gass"
+end
+
+# atau bisa ditulis
+
+puts "gelas kosong" unless gelas
