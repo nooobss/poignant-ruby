c952322750adf1c841136d64999e69096e51b96f
nooop <harisnooop@gmail.com>
2026-02-28 22:25:30 +0700

    mengenal case dan when, seperti switch case js

--- chapt5.rb
+++ chapt5.rb
@@ -1,1 +1,15 @@
 # mengenal def, case, when
+def timeline(year)
+    case year
+    when 1999
+        "The Matrix"
+    when 2000..2009
+        "The Lord of the Rings"
+    when 2010
+        "Inception"
+    else
+        "Unknown"
+    end
+end
+
+puts timeline(2030)
