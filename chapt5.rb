eb2e194ac25969874deeb4309a54d437cd5eb559
nooop <harisnooop@gmail.com>
2026-03-01 00:08:25 +0700

    cara memanggil case-when utk mengevaluasi suatu nilai

--- chapt5.rb
+++ chapt5.rb
@@ -1,4 +1,6 @@
 # mengenal def, case, when
+
+=begin
 def timeline(year)
     case year
     when 1999
@@ -13,3 +15,32 @@ def timeline(year)
 end
 
 puts timeline(2030)
+
+=end
+
+# contoh lain
+
+year = 2022
+
+#menggunakan case-when cara-1
+=begin
+case year
+when 1894
+    puts "lahir"
+when 1895..1913
+    puts "masa kecil"
+else
+    puts "tidak diketahui"
+end
+=end
+#menggunakan case-when cara-2
+bungkus = case year
+when 1894
+    "lahir"
+when 1895..1913
+    "masa kecil"
+else
+    "tidak diketahui"
+end
+
+puts bungkus
